d4350750313e20007b37c808105bf9bb58a88ab5
ScriptKiddy99 <47433174+ScriptKiddy99@users.noreply.github.com>
2019-02-15 23:56:36 +0000

    Add files via upload

---
+++ Format-CounterList.ps1
@@ -0,0 +1,32 @@
+﻿Function Format-CounterList 
+{
+    Param(
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [Array]$CounterSamples
+    )
+   
+   [Array]$CounterObjects = @()
+   [String]$RegexPattern = '(?<ComputerName>(?:\\\\).+)(?<Counterset>(?:\\).+(?:\\))(?<Counter>.+(?:\:))(?<Value>\n?.*)'
+   
+   ForEach($Counter in $CounterSamples) {
+
+        [PSCustomObject]$CounterObj = [PSCustomObject]@{DateTime = (Get-Date -f "dd/MM/yyyy hh:mm:ss tt")}
+
+        [String]$CounterString =  ($Counter.Path + ":" + $Counter.CookedValue)
+        
+        $CounterMatches = [Regex]::Match($CounterString, $RegexPattern)
+                
+        ForEach ($Match in $CounterMatches.Groups) {
+            if ($Match.Name -eq "0") { Continue }
+            
+            Add-Member -InputObject $CounterObj `
+                       -Name $Match.Name `
+                       -Value $Match.Value.Replace("\","").Replace(":","")`
+                       -MemberType NoteProperty
+        }
+        $CounterObjects += $CounterObj
+   }
+   return $CounterObjects
+}
